b62851c3920f2ff3ed98a28c8b07c6711981a2e0
Alex F <adfarries@gmail.com>
2018-10-27 18:54:15 +1100

    Add main.rb

---
+++ main.rb
@@ -0,0 +1,18 @@
+require 'net/ssh'
+require 'net/sftp'
+require 'dotenv/load'
+
+Dotenv.load
+
+# TODO: Split down into discrete classes
+Net::SSH.start(
+  ENV.fetch('HOST'),
+  ENV.fetch('USER'),
+  ENV.fetch('PASSWORD')
+) do |ssh|
+  ssh.sftp.connect do |sftp|
+    Dir.foreach('.') do |file|
+      puts file
+    end
+  end
+end
